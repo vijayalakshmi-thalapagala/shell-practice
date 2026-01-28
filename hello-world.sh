6cc4507bed4729593e0f6b54f2511d5983d6cbf7
Vijayalakshmi Thalapagala <thalapav@amdocs.com>
2026-01-28 19:12:14 +0530

    hello actions

---
+++ hello-world.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "lakshmi:: hi vijaya, how are you?"
+echo "vijaya:: hello lakshmi , I am good.. how are you?"
+echo "lakshmi:: i am also good!!, what's going on?"
+echo "vijaya:: nothing much, U say!!"
