702cda1e42ce93e0cb306a3c8d2ae121132f12ca
Vijayalakshmi Thalapagala <thalapav@amdocs.com>
2026-01-28 19:29:43 +0530

    variables

---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: hi $PERSON2, how are you?"
+echo "$PERSON2:: hello $PERSON1 , I am good.. how are you?"
+echo "$PERSON1:: i am also good $PERSON2!!, what's going on?"
+echo "$PERSON2:: nothing much, U say $PERSON1!!"
