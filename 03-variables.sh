a016bce073c9562ba69f07eeeb4f0acf271d78d9
Vijayalakshmi Thalapagala <thalapav@amdocs.com>
2026-01-28 19:34:00 +0530

    variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -5,6 +5,10 @@ echo "please enter your username::"
 read USER_NAME
 
 echo "user name is $USER_NAME"
+
+echo "Please enter your password"
+
+read  -s PASSWORD
 #echo "$PERSON1:: hi $PERSON2, how are you?"
 #echo "$PERSON2:: hello $PERSON1 , I am good.. how are you?"
 #echo "$PERSON1:: i am also good $PERSON2!!, what's going on?"
