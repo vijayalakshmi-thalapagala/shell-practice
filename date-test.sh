2c2ea4781a2dd80a425dfc85344b18bc13d3b79c
Vijayalakshmi Thalapagala <thalapav@amdocs.com>
2026-01-28 19:42:29 +0530

    variables

--- date-test.sh
+++ date-test.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-TIMESTAMP=$(date)
+START_TIME=$(date +s)
 
-echo "this is current date $TIMESTAMP"
+echo "this is current date $START_TIME"
+
+sleep 10
+
+END_TIME=$(date +s)
+
+echo "this is current date $ENDTIME"
+
+
+DIFF=$(($END_TIME-$START_TIME))
+echo "this is the diff $DIFF"
