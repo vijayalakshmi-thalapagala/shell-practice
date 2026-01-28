ed5c4393e33fe381890d380ed92c2da73c99f5a5
Vijayalakshmi Thalapagala <thalapav@amdocs.com>
2026-01-28 19:49:40 +0530

    variables

--- date-test.sh
+++ date-test.sh
@@ -12,7 +12,7 @@ echo "this is end date $END_TIME"
 
 
 DIFF=$(($END_TIME-$START_TIME))
-echo "this is the diff $DIFF"
+echo "this is the diff $DIFF in seconds"
 
 START=$(date +%H%M%S)
 echo "this is the START $START time"
@@ -24,4 +24,4 @@ echo "this is the END $END time"
 
 DIFF1=$(($START-$END))
 
-echo "this is the expected $DIFF1"
+echo "this is the expected $DIFF1 in seconds"
