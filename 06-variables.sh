f2021a41a6de3892e765e34d9bd3c3a53fc7620d
Vijayalakshmi Thalapagala <thalapav@amdocs.com>
2026-01-29 12:17:35 +0530

    variables

--- 06-variables.sh
+++ 06-variables.sh
@@ -3,9 +3,9 @@
 
 ######### array ##########
 
-my_array=["apples", "banana", "orange", "cherry"]
+my_array=("apples" "banana" "orange" "cherry")
 
-echo "all fruits are here only $my_array[$@]"
-echo "u can eat this $my_array[0] fruit"
-echo "u can eat this $my_array[1] fruit"
-echo "u can eat this $my_array[2] fruit"
+echo "all fruits are here only ${my_array[@]}"
+echo "u can eat this ${my_array[0]} fruit"
+echo "u can eat this ${my_array[1]} fruit"
+echo "u can eat this ${my_array[2]} fruit"
