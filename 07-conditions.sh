f3b6a67c3415e268e482ff74d0707c90b2f64d5d
Vijayalakshmi Thalapagala <thalapav@amdocs.com>
2026-01-29 17:23:34 +0530

    actions

---
+++ 07-conditions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+Number=25
+
+if [$NUMBER -gt 20 ]; then
+
+echo "this $NUMBER greater than 20"
+elif [$NUMBER -eq 20 ]; then
+
+echo "this $NUMBER is equal to 20"
+else
+echo "This $NUMBER is less than to 20"
