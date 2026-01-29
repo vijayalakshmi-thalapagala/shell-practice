538f12dc931ced6e647a9ea9d537b1928574c3f5
Vijayalakshmi Thalapagala <thalapav@amdocs.com>
2026-01-29 19:35:57 +0530

    actions

--- 08-installations.sh
+++ 08-installations.sh
@@ -3,6 +3,35 @@
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
-    echo "please run the commands with sudo access buddy!!"
+    echo "Please run the commands with sudo access buddy!!"
     exit 1
 fi
+
+echo "You are the root user so installing nginx buddy!!"
+dnf install nginx -y
+
+if [$? -ne 0 ]; then
+    echo "Installation nginx .......... Failed!!"
+    exit 1
+else
+    echo "Installation nginx ................ Success !!"
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing MySQL ... failed"
+    exit 1
+else
+    echo "Installing MySQL ... Success"
+fi
+
+#dnf install nodejs -y
+
+#if [ $? -ne 0 ]; then
+#    echo "Installing nodejs ... FAILURE"
+#    exit 1
+#else
+#    echo "Installing nodejs ... SUCCESS"
+#fi
+
